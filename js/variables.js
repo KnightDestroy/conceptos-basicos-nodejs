0b1d44efef9f4493831b821145f3de7c72c62195
KnightDestroy <Jhonrey55@gmail.com>
2023-08-23 09:58:06 -0500

    23/08/2023

---
+++ js/variables.js
@@ -0,0 +1,12 @@
+/**
+    * Los tipos de Datos: String, Number, Boolean, Date, Object,
+    * Para poder declarar Variables: Var, Let, Const (Constante)
+    */
+
+// Datos o Tipos Primitivos
+
+let miNombre = 'Jhon';
+const miNombre2 = 'Edison';
+
+let miEdad = 26;
+console.log(miNombre, miNombre2, miEdad);
