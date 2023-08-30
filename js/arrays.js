72968813ffebaf5116cb9b199b6409468847466f
KnightDestroy <Jhonrey55@gmail.com>
2023-08-30 08:50:17 -0500

    30/08/2023

---
+++ js/arrays.js
@@ -0,0 +1,18 @@
+// Sintaxis del array
+
+let marcasAuto = []; //Estaria declarando un Array Vacío
+
+const marcasAuto2 = [
+    30000,
+    'Mazda',
+    'Toyota',
+    'Ferrari',
+    'Mercedes',
+    'Volkswagen',
+    'Audi',
+    'BMW',
+    'Porsche',
+];
+
+//Mostrar todas las marcas de los Autos
+console.log(marcasAuto2);
